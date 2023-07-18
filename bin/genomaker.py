cf625af9ebc967a3c101380d893bb78ae4e45b55
albertoriva <ariva@ufl.edu>
2023-07-18 17:01:43 -0400

    v0.1

--- bin/genomaker.py
+++ bin/genomaker.py
@@ -35,10 +35,25 @@ class Command():
     name = ""
     file1 = ""
     destination = ""
+    arguments = {}
+    assignments = []
     work_dir = "_gmwork"
 
     def __init__(self, filenum):
         self.destination = self.work_dir + "/file-" + str(filenum)
+
+    def getarg(self, name, default=None):
+        return self.arguments[name] if name in self.arguments else default
+
+    def write_prereq(self, out, *prereqs):
+        out.write("{} : {}".format(self.destination, " ".join(prereqs)))
+        for assg in self.assignments:
+            out.write(" " + assg[1])
+        out.write("\n")
+
+    def write_assignments(self, out):
+        for assg in self.assignments:
+            out.write("\t{}=$$(head -1 {})\n".format(assg[0], assg[1]))
 
 class Cmd_file(Command):
     name = "file"
@@ -46,7 +61,7 @@ class Cmd_file(Command):
     def example(self):
         return "a = file filename.txt"
 
-    def parse(self, file1, args, bindings):
+    def parse(self, file1, bindings):
         self.destination = file1
 
     def write(self, out):
@@ -54,8 +69,50 @@ class Cmd_file(Command):
 
 class Cmd_filter(Command):
     name = "filter"
-    column = 4
     test = ""
+    hdr = ""
+
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.destination = self.getarg("as", self.destination)
+        self.test = self.getarg("test")
+        self.hdr = self.getarg("header")
+
+    def write(self, out):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        if self.hdr == "keep":
+            h = "-P"
+        elif self.hdr == "remove":
+            h = "-p"
+        else:
+            h = ""
+        out.write("\ttcalc.py {} \"filter {}\" {} > {}\n\n".format(h, self.test, self.file1, self.destination))
+
+class Cmd_assoc(Command):
+    name = "assoc"
+    file2 = ""
+    incol = "1"
+    outcol = None
+    missing = ""
+
+    def example(self):
+        return "a = assoc file.txt with file2.txt in 3 out 4 missing NA"
+
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.file2 = vval(self.getarg("with"), bindings)
+        self.destination = self.getarg("as", self.destination)
+        self.incol = self.getarg("in") or "1"
+        self.outcol = self.getarg("out")
+        self.missing = self.getarg("missing")
+
+    def write(self, out):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        oc = "-o " + self.outcol if self.outcol else "-w"
+        mi = "-m " + self.missing if self.missing else "-x"
+        out.write("\tcut -f 1 {} | assoc.py -i {} {} {} {} > {}\n\n".format(self.file1, self.incol, oc, mi, self.file2, self.destination))
 
 class Cmd_intersect(Command):
     name = "intersect"
@@ -64,15 +121,14 @@ class Cmd_intersect(Command):
     def example(self):
         return "a = intersect file1.txt with file2.txt as file3.txt"
 
-    def parse(self, file1, args, bindings):
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        if "with" in args:
-            self.file2 = vval(args["with"], bindings)
-        if "as" in args:
-            self.destination = args["as"]
+        self.file2 = vval(self.getarg("with"), bindings)
+        self.destination = self.getarg("as", self.destination)
 
     def write(self, out):
-        out.write("{} : {} {}\n".format(self.destination, self.file1, self.file2))
+        self.write_prereq(out, self.file1, self.file2)
+        self.write_assignments(out)
         out.write("\tbedtools intersect -a {} -b {} | sort -k 1,1 -k 2,2n | bedtools merge -i - > {}\n\n".format(self.file1, self.file2, self.destination))
 
 class Cmd_subtract(Command):
@@ -82,14 +138,14 @@ class Cmd_subtract(Command):
     def example(self):
         return "a = subtract file2.txt from file1.txt"
 
-    def parse(self, file1, words, bindings):
+    def parse(self, file1, bindings):
         self.file2 = vval(file1, bindings)
-        self.file1 = vval(words["from"], bindings)
-        if "as" in words:
-            self.destination = words["as"]
+        self.file1 = vval(self.getarg("from"), bindings)
+        self.destination = self.getarg("as", self.destination)
 
     def write(self, out):
-        out.write("{} : {} {}\n".format(self.destination, self.file1, self.file2))
+        self.write_prereq(out, self.file1, self.file2)
+        self.write_assignments(out)
         out.write("\tbedtools intersect -v -a {} -b {} | sort -k 1,1 -k 2,2n | bedtools merge -i - > {}\n\n".format(self.file1, self.file2, self.destination))
 
 class Cmd_merge(Command):
@@ -99,14 +155,14 @@ class Cmd_merge(Command):
     def example(self):
         return "a = merge file1.txt with file2.txt"
 
-    def parse(self, file1, words, bindings):
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        self.file2 = vval(words["with"], bindings)
-        if "as" in words:
-            self.destination = words["as"]
+        self.file2 = vval(self.getarg("with"), bindings)
+        self.destination = self.getarg("as", self.destination)
 
     def write(self, out):
-        out.write("{} : {} {}\n".format(self.destination, self.file1, self.file2))
+        self.write_prereq(out, self.file1, self.file2)
+        self.write_assignments(out)
         out.write("\tcat {} {} | sort -k 1,1 -k 2,2n | bedtools merge -i - > {}\n\n".format(self.file1, self.file2, self.destination))
 
 class Cmd_cut(Command):
@@ -116,51 +172,73 @@ class Cmd_cut(Command):
     def example(self):
         return "a = cut file1.txt columns 1,3,2,5"
 
-    def parse(self, file1, args, bindings):
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        if "as" in args:
-            self.destination = args["as"]
-        for k in ["cols", "columns"]:
-            if k in args:
-                self.columns = args[k]
-                break
+        self.destination = self.getarg("as", self.destination)
+        self.columns = self.getarg("cols") or self.getarg("columns")
 
     def write(self, out):
-        out.write("{} : {}\n".format(self.destination, self.file1))
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
         out.write("\tkut -f {} {} > {}\n\n".format(self.columns, self.file1, self.destination))
 
 class Cmd_paste(Command):
     name = "paste"
     otherfiles = []
+    column = 1
 
     def example(self):
         return "a = paste file1.txt with file2.txt,file3.txt,file4.txt"
 
-    def parse(self, file1, args, bindings):
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        if "as" in args:
-            self.destination = args["as"]
-        if "with" in args:
-            self.otherfiles = [ vval(f, bindings) for f in args["with"].split(",") ]
+        self.destination = self.getarg("as", self.destination)
+        self.columns = self.getarg("cols") or self.getarg("columns") or 1
+        if "with" in self.arguments:
+            self.otherfiles = [ vval(f, bindings) for f in self.arguments["with"].split(",") ]
 
     def write(self, out):
-        out.write("{} : {} {}\n".format(self.destination, self.file1, " ".join(self.otherfiles)))
-        out.write("\tpazte -o {} -F {} {}\n\n".format(self.destination, self.file1, " ".join(self.otherfiles)))
+        self.write_prereq(out, self.file1, *self.otherfiles)
+        self.write_assignments(out)
+        out.write("\tpazte -o {} -F -c {} {} {}\n\n".format(self.destination, self.column, self.file1, " ".join(self.otherfiles)))
+
+class Cmd_header(Command):
+    name = "header"
+    remove = 0
+    colnames = ""
+
+    def example(self):
+        return "a = header b colnames name1,name2,name3"
+    
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.destination = self.getarg("as", self.destination)
+        self.remove = self.getarg("remove", self.remove)
+        if "colnames" in self.arguments:
+            self.colnames = "\\t".join(self.arguments["colnames"].split(","))
+
+    def write(self, out):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        if self.colnames:
+            out.write('\techo -e "{}" > {}\n'.format(self.colnames, self.destination))
+        if self.remove:
+            out.write("\ttail -n +{} {} >> {}\n\n".format(self.remove, self.file1, self.destination))
+        else:
+            out.write("\tcat {} >> {}\n\n".format(self.file1, self.destination))
 
 class Cmd_bedtocoords(Command):
     name = "bedtocoords"
     column = 1
-    def parse(self, file1, args, bindings):
+
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        if "as" in args:
-            self.destination = args["as"]
-        for k in ["col", "column"]:
-            if k in args:
-                self.column = args[k]
-                break
+        self.destination = self.getarg("as", self.destination)
+        self.columns = self.getarg("cols") or self.getarg("columns")
         
     def write(self, out):
-        out.write("{} : {}\n".format(self.destination, self.file1))
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
         out.write("\tgmutils.py bedtocoords -c {} {} > {}\n\n".format(self.column, self.file1, self.destination))
 
 class Cmd_subintervals(Command):
@@ -170,43 +248,164 @@ class Cmd_subintervals(Command):
     def example(self):
         return "a = subintervals file1.txt size 50000 as file2.txt"
 
-    def parse(self, file1, args, bindings):
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        if "as" in args:
-            self.destination = args["as"]
-        if "size" in args:
-            self.size = int(args["size"])
+        self.destination = self.getarg("as", self.destination)
+        self.size = self.getarg("size") or self.size
 
     def write(self, out):
-        out.write("{} : {}\n".format(self.destination, self.file1))
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
         out.write("\tgmutils.py subintervals -s {} {} > {}\n\n".format(self.size, self.file1, self.destination))
 
 class Cmd_quantify(Command):
     name = "quantify"
     bedfile = ""
+    norm = False
 
     def example(self):
-        return "a = quantify file1.bam on file2.bed as file3.txt"
+        return "a = quantify file1.bam on file2.bed as file3.txt norm 1234567"
 
-    def parse(self, file1, args, bindings):
+    def parse(self, file1, bindings):
         self.file1 = vval(file1, bindings)
-        if "as" in args:
-            self.destination = args["as"]
-        self.bedfile = vval(args["on"], bindings)
+        self.destination = self.getarg("as", self.destination)
+        self.norm = self.getarg("norm")
+        self.bedfile = vval(self.getarg("on"), bindings)
 
     def write(self, out):
-        out.write("{} : {} {}\n".format(self.destination, self.file1, self.bedfile))
-        out.write("\tsamtools bedcov {} {} > {}\n\n".format(self.bedfile, self.file1, self.destination))
+        self.write_prereq(out, self.file1, self.bedfile)
+        self.write_assignments(out)
+        if self.norm:
+            out.write("\tsamtools bedcov {} {} | gmutils.py scale {} > {}\n\n".format(self.bedfile, self.file1, self.norm, self.destination))
+        else:
+            out.write("\tsamtools bedcov {} {} > {}\n\n".format(self.bedfile, self.file1, self.destination))
+
+class Cmd_adjust(Command):
+    name = "adjust"
+    spec = ""
+
+    def example(self):
+        return "a = adjust file1.bed left 100 right 200"
+
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.destination = self.getarg("as", self.destination)
+        if "left" in self.arguments:
+            self.spec += " -l " + self.arguments["left"]
+        if "right" in self.arguments:
+            self.spec += " -r " + self.arguments["right"]
+        if "upstream" in self.arguments:
+            self.spec += " -u " + self.arguments["upstream"]
+        if "downstream" in self.arguments:
+            self.spec += " -d " + self.arguments["downstream"]
+        if "column" in self.arguments:
+            self.spec += " -c " + self.arguments["column"]
+        if "col" in self.arguments:
+            self.spec += " -c " + self.arguments["col"]
+        if "strandcol" in self.arguments:
+            self.spec += " -s " + self.arguments["strandcol"]
+        if "mode" in self.arguments:
+            self.spec += " -m " + self.arguments["mode"]
+        
+    def write(self, out):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        out.write("\tgmutils.py adjust {} {} > {}\n\n".format(self.spec, self.file1, self.destination))
+
+# Commands that extract values from files
+
+class Cmd_nreads(Command):
+    name = "nreads"
+    
+    def example(self):
+        return "a = nreads file1.bam"
+
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.destination = self.getarg("as", self.destination)
+
+    def write(self, out):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        out.write("\tsamtools idxstats {} | tcalc.py -q \"do sum(C3)\" > {}\n\n".format(self.file1, self.destination))
+
+class Cmd_nlines(Command):
+    name = "nlines"
+    
+    def example(self):
+        return "a = nlines file1.txt"
+
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.destination = self.getarg("as", self.destination)
+
+    def write(self, out):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        out.write("\tgrep -c ^ {} > {}\n\n".format(self.file1, self.destination))
+
+## Operations on sets of objects
+
+class Cmd_setmerge(Command):
+    name = "setmerge"
+    file2 = ""
+    col1 = 1
+    col2 = 1
+
+    def example(self):
+        return "a = setmerge file1 col1 3 with file2 col2 5"
+
+    def parse(self, file1, bindings):
+        self.file1 = vval(file1, bindings)
+        self.file2 = vval(self.getarg("with"), bindings)
+        self.destination = self.getarg("as", self.destination)
+        self.col1 = self.getarg("col1", default="1")
+        self.col2 = self.getarg("col2", default="1")
+
+    def write(self, out):
+        self.write_generic(out, "-u")
+
+    def write_generic(self, out, op):
+        self.write_prereq(out, self.file1)
+        self.write_assignments(out)
+        out.write("\tcolx.py -w {} {}:{} {}:{} > {}\n\n".format(op, self.file1, self.col1, self.file2, self.col2, self.destination))
+
+class Cmd_setdifference(Cmd_setmerge):
+    name = "setdifference"
+
+    def example(self):
+        return "a = setdifference file1 col1 3 with file2 col2 5"
+
+    def write(self, out):
+        self.write_generic(out, "-d")
+
+class Cmd_setintersect(Cmd_setmerge):
+    name = "setintersect"
+
+    def example(self):
+        return "a = setintersect file1 col1 3 with file2 col2 5"
+
+    def write(self, out):
+        self.write_generic(out, "-i")
 
 COMMANDS = {"file": Cmd_file,
+            "filter": Cmd_filter,
+            "assoc" : Cmd_assoc,
             "intersect": Cmd_intersect,
             "subtract": Cmd_subtract,
             "merge": Cmd_merge,
             "cut": Cmd_cut,
             "paste": Cmd_paste,
+            "header": Cmd_header,
             "subintervals": Cmd_subintervals,
             "quantify": Cmd_quantify,
             "bedtocoords": Cmd_bedtocoords,
+            "adjust": Cmd_adjust,
+            "nreads": Cmd_nreads,
+            "nlines": Cmd_nlines,
+            "setmerge": Cmd_setmerge,
+            "setdifference": Cmd_setdifference,
+            "setintersect": Cmd_setintersect,
 }
 
 ### Main application class
@@ -215,9 +414,10 @@ class GenoMaker():
     scriptfile = None
     variables = {}
     commands = []
+    targets = []
     filenum = 1
 
-    def parseArgs(args):
+    def parseArgs(self, args):
         prev = ""
         for a in args:
             if prev == "-w":
@@ -237,21 +437,51 @@ class GenoMaker():
                     continue
                 self.parseCommand(line)
 
+    def parseAssignments(self, line):
+        asslist = []
+        p1 = line.find("{")
+        p2 = line.find("}")
+        if (p1 > 0) and (p2 > 0) and (p2 > p1):
+            clean_line = line[:p1] + line[p2+1:]
+            assignments = line[p1+1:p2].split(";")
+            for assg in assignments:
+                if "=" in assg:
+                    parts = assg.split("=")
+                    var = parts[0].strip()
+                    source = vval(parts[1].strip(), self.variables)
+                    asslist.append((var, source))
+            return True, clean_line, asslist
+        elif (p1 > 0) or (p2 > 0):
+            return False, "", []
+        else:
+            return True, line, []
+
     def parseCommand(self, line):
-        words = line.split()
-        if len(words) > 3 and words[1] == '=':
-            cmdname = words[2]
-            if cmdname in COMMANDS:
-                cmdclass = COMMANDS[cmdname]
-                cmd = cmdclass(self.filenum)
-                args = parse_arguments(words[4:])
-                cmd.parse(words[3], args, self.variables)
-                self.commands.append(cmd)
-                self.variables[words[0]] = cmd.destination
-                self.filenum += 1
-                #print(self.variables)
-            else:
-                sys.stderr.write("Unknown command `{}'\n".format(cmdname))
+        assignments = []
+        good = True
+        if "{" in line:
+            good, line, assignments = self.parseAssignments(line)
+
+        if good:
+            words = line.split()
+            if len(words) > 2 and words[0] == 'target':
+                self.targets.append(words[1:])
+            elif len(words) > 3 and words[1] == '=':
+                cmdname = words[2]
+                if cmdname in COMMANDS:
+                    cmdclass = COMMANDS[cmdname]
+                    cmd = cmdclass(self.filenum)
+                    cmd.assignments = assignments
+                    cmd.arguments = parse_arguments(words[4:])
+                    cmd.parse(words[3], self.variables)
+                    self.commands.append(cmd)
+                    self.variables[words[0]] = cmd.destination
+                    self.filenum += 1
+                    #print(self.variables)
+                else:
+                    sys.stderr.write("Unknown command `{}'\n".format(cmdname))
+        else:
+            sys.stderr.write("Cannot parse line `{}'\n".format(line))
 
     def run(self):
         self.parseScript(self.scriptfile)
@@ -259,6 +489,9 @@ class GenoMaker():
         sys.stdout.write(".ONESHELL : \n\n")
         for cmd in self.commands:
             cmd.write(sys.stdout)
+        for tg in self.targets:
+            tgs = [vval(t, self.variables) for t in tg[1:]]
+            sys.stdout.write("{} : {}\n\n".format(tg[0], " ".join(tgs)))
 
 def main(args):
     GM = GenoMaker()
@@ -280,3 +513,4 @@ if __name__ == "__main__":
 # x = filter a test "C3>0"
 # quantify
 # quartiles
+# $p = 
--- bin/gmutils.py
+++ bin/gmutils.py
@@ -2,6 +2,105 @@
 
 import sys
 import csv
+import numpy as np
+
+class Adjust():
+    infile = ""
+    column = 0
+    strandcol = None
+    mode = "b"                  # or l, r
+    left = 0
+    right = 0
+    upstream = 0
+    downstream = 0
+    _func = None
+
+    def parseArgs(self, args):
+        prev = ""
+        for a in args:
+            if prev == "-l":
+                self.left = int(a)
+                prev = ""
+            elif prev == "-r":
+                self.right = int(a)
+                prev = ""
+            elif prev == "-u":
+                self.upstream = int(a)
+                prev = ""
+            elif prev == "-d":
+                self.downstream = int(a)
+                prev = ""
+            elif prev == "-c":
+                self.column = int(a)-1
+                prev = ""
+            elif prev == "-s":
+                self.strandcol = int(a)-1
+                prev = ""
+            elif prev == "-m":
+                self.mode = a
+                prev = ""
+            elif a in ["-l", "-r", "-u", "-d", "-c", "-s", "-m"]:
+                prev = a
+            else:
+                self.infile = a
+
+        if self.strandcol:
+            if self.mode == 'l':
+                self._func = self.sl
+            elif self.mode == 'r':
+                self._func = self.sr
+            else:
+                self._func = self.sb
+        else:
+            if self.mode == 'l':
+                self._func = self.ul
+            elif self.mode == 'r':
+                self._func = self.ur
+            else:
+                self._func = self.ub
+
+    def ub(self, a, b, row):
+        return a + self.left, b + self.right
+
+    def ul(self, a, b, row):
+        return a + self.left, a + self.right
+
+    def ur(self, a, b, row):
+        return b + self.left, b + self.right
+
+    def sb(self, a, b, row):
+        strand = row[self.strandcol]
+        if strand == "+":
+            return a + self.upstream, b + self.downstream
+        else:
+            return a + self.downstream, b + self.upstream
+
+    def sl(self, a, b, row):
+        strand = row[self.strandcol]
+        if strand == "+":
+            return a + self.upstream, a + self.downstream
+        else:
+            return b + self.downstream, b + self.upstream
+
+    def sr(self, a, b, row):
+        strand = row[self.strandcol]
+        if strand == "+":
+            return b + self.upstream, b + self.downstream
+        else:
+            return a + self.downstream, a + self.upstream
+
+    def run(self):
+        with open(self.infile, "r") as f:
+            c = csv.reader(f, delimiter='\t')
+            for row in c:
+                start = int(row[self.column+1])
+                end = int(row[self.column+2])
+                newstart, newend = self._func(start, end, row)
+                newstart = max(newstart, 0)
+                row[self.column+1] = str(newstart)
+                row[self.column+2] = str(newend)
+                sys.stdout.write("\t".join(row) + "\n")
+        
 
 class Subintervals():
     infile = ""
@@ -33,7 +132,7 @@ class Subintervals():
                         break
                     else:
                         sys.stdout.write("{}\t{}\t{}\n".format(chrom, a, b))
-                        a = b
+                        a = b+1
                         b += self.size
 
 class bedToCoords():
@@ -71,9 +170,69 @@ class coordsToBed(bedToCoords):
                 outrow = row[:self.column] + [chrom, start, end] + row[self.column+3:]
                 sys.stdout.write("\t".join(outrow) + "\n")
 
+class Correlation():
+    infile = ""
+    col1 = 0
+    col2 = 1
+    
+    def parseArgs(self, args):
+        prev = ""
+        for a in args:
+            if prev == "-x":
+                self.col1 = int(a) - 1
+                prev = ""
+            elif prev == "-y":
+                self.col2 = int(a) - 1
+                prev = ""
+            elif a in ["-x", "-y"]:
+                prev = a
+            else:
+                self.infile = a
+
+    def run(self):
+        xs = []
+        ys = []
+        with open(self.infile, "r") as f:
+            c = csv.reader(f, delimiter='\t')
+            for row in c:
+                xs.append(float(row[self.col1]))
+                ys.append(float(row[self.col2]))
+        r = np.corrcoef(xs, ys)
+        n = len(xs)
+        sys.stdout.write("{}:{}:{}\t{}\t{}\t{}\t{}\n".format(self.infile, self.col1+1, self.col2+1, n, sum(xs)/n, sum(ys)/n, r[0,1]))
+
+class Scale():
+    column = 3
+    factor = 10000000
+    value = 1
+
+    def parseArgs(self, args):
+        prev = ""
+        for a in args:
+            if prev == "-c":
+                self.column = int(a) - 1
+                prev = ""
+            elif prev == "-f":
+                self.factor = int(a)
+                prev = ""
+            elif a in ["-c", "-f"]:
+                prev = a
+            else:
+                self.value = int(a)
+
+    def run(self):
+        sf = self.factor / self.value
+        c = csv.reader(sys.stdin, delimiter='\t')
+        for row in c:
+            row[self.column] = str(float(row[self.column]) * sf)
+            sys.stdout.write("\t".join(row) + "\n")
+
 COMMANDS = {"subintervals": Subintervals,
             "bedtocoords": bedToCoords,
             "coordstobed": coordsToBed,
+            "correl": Correlation,
+            "adjust": Adjust,
+            "scale": Scale
 }
 
 def main(args):
